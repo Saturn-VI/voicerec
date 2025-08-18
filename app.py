714d7b57d0624ca35cc78e483d648928e3e8853e
saturnvi <maxwang420@gmail.com>
2025-08-18 11:21:23 -0500

    get most of create account

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
+import base64
+import binascii
+import io
 from dataclasses import dataclass
 from pathlib import Path
-from litestar import Litestar, get, post
+
+import bcrypt
+import torchaudio
+import torchaudio.transforms as T
+from litestar import Litestar, Response, get, post
+from litestar.di import Provide
 from litestar.static_files import create_static_files_router
+from litestar.status_codes import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from litestar.stores.file import FileStore
 from torch import Tensor
+
+from ml import EmbeddingGenerator
+from singer_identity.model import IdentityEncoder, load_model
 
 HTML_DIR = Path("website")
 
@@ -19,28 +31,81 @@ HTML_DIR = Path("website")
 class CredentialData:
     username: str
     password: str
-    audio_data: str # should be base64 encoded audio data
+    audio_data: str # should be base64 encoded audio data as webm
 
 @dataclass
 class User:
     username: str
-    password: str # should be hashed
+    password: bytes # should be hashed
     embedding: Tensor
 
-file_store: FileStore = FileStore(Path("database.db"))
+file_store: FileStore
+embedding_generator: EmbeddingGenerator
+
+def embedding_generator_provider() -> EmbeddingGenerator:
+    global embedding_generator
+    return embedding_generator
+
+def file_store_provider() -> FileStore:
+    global file_store
+    return file_store
 
 async def on_startup() -> None:
+    path = Path("database")
+    path.mkdir(parents=True, exist_ok=True)
+    file_store = FileStore(path, create_directories=True)
     await file_store.delete_expired()
+
+    model = load_model("byol")
+    if (isinstance(model, IdentityEncoder)):
+        global embedding_generator
+        embedding_generator = EmbeddingGenerator(model)
+    else:
+        raise ValueError("Model is not an IdentityEncoder")
 
 @get("/hello")
 async def hello() -> str:
     return "Hello, World!"
 
 # create account
-# takes
-@post("/account/create")
-async def account_create(data: CredentialData) -> str:
-    return "TODO"
+@post("/account/create", status_code=HTTP_201_CREATED, dependencies={"embedding_generator": Provide(embedding_generator_provider), "file_store": Provide(file_store_provider)})
+async def account_create(data: CredentialData) -> Response[str]:
+    if (not data.username or not isinstance(data.username, str)):
+        return Response("Invalid username", status_code=HTTP_400_BAD_REQUEST)
+
+    file_store = file_store_provider()
+    if file_store.exists(data.username):
+        return Response("Username already exists", status_code=HTTP_400_BAD_REQUEST)
+
+    embedding_generator = embedding_generator_provider()
+
+    try:
+        webm_bytes = base64.b64decode(data.audio_data)
+    except binascii.Error as e:
+        print("Error decoding base64 audio data:", e)
+        return Response("Invalid base64 audio data", status_code=HTTP_400_BAD_REQUEST)
+
+    webm_stream = io.BytesIO(webm_bytes)
+
+    try:
+        wav, sample_rate = torchaudio.load(webm_stream, format='webm')
+    except Exception as e:
+        print("Error loading audio data:", e)
+        return Response("Invalid audio data format", status_code=HTTP_400_BAD_REQUEST)
+
+    if sample_rate != 44100:
+        print("Sample rate not 44100, resampling")
+        resampler = T.Resample(orig_freq=sample_rate, new_freq=44100)
+        wav = resampler(wav)
+
+    user: User = User(
+        username = data.username,
+        password = bcrypt.hashpw(data.password.encode('utf-8'), bcrypt.gensalt()),
+        embedding = embedding_generator.generate_embedding(wav)
+    )
+    await file_store.set(user.username, user)
+    print(user)
+    return Response("Account created successfully", status_code=HTTP_201_CREATED)
 
 @post("/account/login")
 async def account_login(data: CredentialData) -> str:
@@ -56,5 +121,7 @@ app = Litestar(
             directories=[HTML_DIR],
             html_mode=True,
         ),
-    ]
+    ],
+    dependencies={"embedding_generator": Provide(embedding_generator_provider), "file_store": Provide(file_store_provider)},
+    on_startup=[on_startup],
 )
